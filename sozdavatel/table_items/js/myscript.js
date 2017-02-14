9b5e3fde3ed443fb6303555366e0f28219363e48
dmitriydudnikov <iamdmitriydudnikov@gmail.com>
2017-02-14 22:23:31 +0400

    Добавил скрывающий класс, добавил его в html и изменил скрипт.

--- sozdavatel/table_items/js/myscript.js
+++ sozdavatel/table_items/js/myscript.js
@@ -1,9 +1,13 @@
 $(document) .ready(function() {
 	$('#list').click(function(){
 		$('.item').addClass('list');
+		$('#table').removeClass('hidden');
+		$('#list').addClass('hidden');
 	});
 
 	$('#table').click(function(){
 		$('.item').removeClass('list');
+		$('#table').addClass('hidden');
+		$('#list').removeClass('hidden');
 	});
 });
